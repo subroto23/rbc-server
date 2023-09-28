53ab61c51d57ee4f5a052724a81974337484fb7c
Subroto Das <rupashi.bangla.club@gmail.com>
2023-09-28 08:26:25 +0600

    front page Show

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require("./src/app");
 const mongodbDatabaseConnection = require("./src/config/db");
 const { serverPort } = require("./src/secret");
 
+app.get("/", (req, res, next) => {
+  res.json("Hellow Server Side");
+});
+
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:${serverPort}`);
 
