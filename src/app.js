2d94993655111e9f9b266b824ddec4c98077be6b
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-04 08:02:12 +0600

    too many reques change

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(cors());
 //Express limits setup
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  limit: 400, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: "Too many requests from this Ip.please try again later",
 });
 
