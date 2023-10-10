5c4659590b88f29ff999def94b030beb5fb6dc71
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-10 12:08:09 +0600

    Parse Data

--- src/Controller/UsersControler/getUserById.js
+++ src/Controller/UsersControler/getUserById.js
@@ -2,13 +2,13 @@ const createHttpError = require("http-errors");
 const { handleSuccess } = require("../../Services/SuccessError");
 const FindById = require("../../helper/FindDataById");
 const userSchemaModel = require("../../Modele/UsersModel/UsersModel");
+const { json } = require("body-parser");
 
 const getUserById = async (req, res, next) => {
   try {
-    const email = req.body;
-
+    const {email }= req.body;
     //Function  Call get Data By ID
-    const user = await userSchemaModel.findOne({ email: email });
+    const user = await userSchemaModel.find({ email });
 
     return handleSuccess(res, {
       statusCode: 201,
@@ -18,7 +18,8 @@ const getUserById = async (req, res, next) => {
       },
     });
   } catch (error) {
-    next(createHttpError(404, "Id Based user not Found,Internal Error"));
+    console.log(error.message);
+    // next(createHttpError(404, "Id Based user not Found,Internal Error"));
   }
 };
 module.exports = getUserById;
