70e21c3f6032cfed464d6dbffab9e000b62417f2
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-13 12:41:24 +0600

    Event Get

---
+++ src/Controller/EventsController/GetController.js
@@ -0,0 +1,18 @@
+const createHttpError = require("http-errors");
+const EventsModel = require("../../Modele/EventModel/EventModel");
+const { handleSuccess } = require("../../Services/SuccessError");
+
+const getEventController = async (req, res, next) => {
+  try {
+    const event = await EventsModel.findOne();
+    if (!event) {
+      createHttpError("এই মুহুর্তে কোনো উৎসবের নাম খুজে পাওয়া যাচ্ছে না।");
+    }
+    return handleSuccess(res, {
+      statusCode: 201,
+      message: "আপনার অনুরোধ সফল হয়েছে",
+      payload: { event },
+    });
+  } catch (error) {}
+};
+module.exports = { getEventController };
--- src/Router/EventRoute/EventsRoute.js
+++ src/Router/EventRoute/EventsRoute.js
@@ -4,7 +4,10 @@ const {
   eventsPostController,
 } = require("../../Controller/EventsController/EventController");
 const upload = require("../../Middleware/UploadFile");
+const {
+  getEventController,
+} = require("../../Controller/EventsController/GetController");
 
+eventRoute.get("/", getEventController);
 eventRoute.post("/create", upload.single("image"), eventsPostController);
-
 module.exports = eventRoute;
