5a00a8896e489d9aaf7cc8cf5b47e536dbea50b8
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-16 09:34:06 +0600

    Due Model With Update

--- src/Controller/DueController/DueUpdateController.js
+++ src/Controller/DueController/DueUpdateController.js
@@ -1,20 +1,20 @@
 const createHttpError = require("http-errors");
-const Cada = require("../../Modele/Cada/CadaModel");
 const FindById = require("../../helper/FindDataById");
 const { handleSuccess } = require("../../Services/SuccessError");
+const DueModel = require("../../Modele/DueModel/DueModel");
 
 const DueUpdateController = async (req, res, next) => {
   try {
     const id = req.params.id;
     const { paidTk, due } = req.body;
-    const isData = await FindById(Cada, id);
+    const isData = await FindById(DueModel, id);
     if (!isData) {
       throw createHttpError("আইডি পাওয়া যায় নি");
     }
     const filter = isData;
     const update = { $set: { paidTk, due } };
     const options = { new: true };
-    const Updated = await Cada.findByIdAndUpdate(filter, update, options);
+    const Updated = await DueModel.findByIdAndUpdate(filter, update, options);
     return handleSuccess(res, {
       statusCode: 201,
       message: "আপডেট করা হয়েছে",
