dca480c89ab4c4803bc3242f213ef48d6102ae99
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-17 10:48:23 +0600

    Pagination

--- src/Controller/NewsController/NewsGetController.js
+++ src/Controller/NewsController/NewsGetController.js
@@ -3,14 +3,26 @@ const NewsModel = require("../../Modele/NewsModel/NewsModel");
 const { handleSuccess } = require("../../Services/SuccessError");
 const newsGetController = async (req, res, next) => {
   try {
-    const allNews = await NewsModel.find();
+    const page = Number(req.queary.page) || 1;
+    const limit = Number(req.queary.limit) || 3;
+    const allNews = await NewsModel.find()
+      .limit(limit)
+      .skip((page - 1) * 3);
     if (!allNews) {
       throw createHttpError("এই মুহুর্তে ডাটাবেজে কোনো নিউজ নেই");
     }
+
+    const count = await NewsModel.find().countDocuments();
     return handleSuccess(res, {
       statusCode: 201,
       message: "All News Successfully returned",
-      payload: { allNews },
+      payload: {
+        allNews,
+        totalPages: Math.ceil(count / limit),
+        currentPage: page,
+        previousPage: page - 1 > 0 ? page - 1 : null,
+        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+      },
     });
   } catch (error) {
     next(error);
